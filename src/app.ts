2e797439cd4180d9d8b798a02a1b9dcc43634069
Igor Plath <igorplath.martins.stz@gmail.com>
2024-02-02 15:38:42 -0300

    Criando rota

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,9 @@
 import express from "express";
 import cors from "cors"
+import { Ave } from "./model/Ave";
+import { Mamifero } from "./model/Mamifero"
+import { Reptil } from "./model/Reptil"
+
 
 const port = 3001;
 
@@ -8,10 +12,19 @@ const server = express();
 server.use(express.json());
 server.use(cors());
 
-server.get('/', (req, res) =>
-    res.json('Olá mundo!!')
-);
+server.get('/', (req, res) => {
+    let ave: Ave = new Ave('Papagaio', 30, 'Masculino', 10);
+    let reptil: Reptil = new Reptil('Lagarto', 2, 'Femea', 'Cicloides');
+    let mamifero: Mamifero = new Mamifero('Doberman', 'Cachorro', 102, 'Femea')
+    res.json([ave, reptil, mamifero])
+});
 
 server.listen(port, () => {
     console.log(`servidor está escutando no endereço http://localhost:${port}`);
 });
+
+server.post ('/ave', (req, res) => {
+    const { nome, idade, genero, envergadura } = req.body;
+    const novaAve = new Ave(nome, idade, genero, envergadura);
+    res.json(['Está é a nova ave do Zoológico', novaAve]);
+})
--- src/model/Ave.ts
+++ src/model/Ave.ts
@@ -1,10 +1,13 @@
-class Ave extends Animal {
+import {Animal} from "./Animal";
+
+export class Ave extends Animal {
     private envergadura: number;
 
-    constructor(_envergadura: number,
+    constructor(
         _nome: string,
         _idade: number,
-        _genero: string) {
+        _genero: string,
+        _envergadura: number,) {
         super(_nome, _idade, _genero);
         this.envergadura = _envergadura
     }
